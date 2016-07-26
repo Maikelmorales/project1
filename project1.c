9cc1b7472cf7ae33aaf5179cef807f466829de46
Maikelmorales <maikel.morales@unet.edu.ve>
2016-07-26 10:26:24 -0400

    Add files via upload

---
+++ project1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b, c
+    printf("valor de a= ");
+    scanf("a=%d", &a);
+    printf("Valor de b= ");
+    scanf("b=%d", &b);
+    printf("Valor de c= ");
+    scanf("C=%D", &C);
+    if(a<b&b, b<c);
+    {
+	printf("%d, %d, %d", a, b, c);
+	}
+	else{
+		if(b<a&a, a<c);
+		Printf("%d, %d, %d", a, b, c);
+	}
+	else{
+		if(c<b&b, b<c);
+		printf("%d, %d, %d", a, b, c);
+	}
+	return 0;
+}
